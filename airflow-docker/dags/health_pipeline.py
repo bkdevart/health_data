06971a12af7a38a66d1b12470f6a0280f6c3eaef
Brandon Knox <bknox.dev@gmail.com>
2024-08-06 09:20:53 -0700

    added branching logic that checks for export.xml file

--- airflow-docker/dags/health_pipeline.py
+++ airflow-docker/dags/health_pipeline.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import psycopg2
 import glob
+import json
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
@@ -10,6 +11,8 @@ from airflow.operators.bash import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.sensors.filesystem import FileSensor
 from airflow.models.connection import Connection
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
 
 default_args = {
    'owner': 'admin'
@@ -124,6 +127,17 @@ def export_combined_data():
     df =  pd.read_sql('SELECT * FROM combined_data', conn)
     df.to_csv('output/combined_data.csv', index=False)
 
+def check_for_xml(**kwargs):
+    condition = checking_for_xml_file
+    if condition:
+        return 'xml_present'
+    else:
+        return 'xml_absent'
+    
+def parse_xml_file():
+    # TODO: move over XML parse code
+    print('found XML file!')
+
 with DAG(
     dag_id = 'health_db_pipeline',
     description = 'Running a pipeline using a file sensor',
@@ -132,8 +146,21 @@ with DAG(
     schedule_interval = '@once',
     tags = ['pipeline', 'sensor', 'file sensor'],
     template_searchpath = '/opt/airflow/sql'
-
 ) as dag:
+    
+    # xml_present = DummyOperator(task_id='xml_present')
+    # xml_absent = DummyOperator(task_id='xml_absent')
+
+    branch_op = BranchPythonOperator(
+        task_id='branch_task_xml',
+        python_callable=check_for_xml
+    )
+
+    parse_xml_data = PythonOperator(
+        task_id = 'xml_present',
+        python_callable = parse_xml_file
+    )
+
     create_table_cycling = PostgresOperator(
         task_id = 'create_table_cycling',
         postgres_conn_id = 'postgres_health_db',
@@ -158,25 +185,32 @@ with DAG(
         sql = 'create_table_combined_data.sql'
     )
 
+    checking_for_xml_file = FileSensor(
+        task_id = 'checking_for_xml_file',
+        filepath = 'tmp/export.xml',
+        poke_interval = 10,
+        timeout = 60 * 10
+    )
+
     checking_for_cycling_file = FileSensor(
         task_id = 'checking_for_cycling_file',
         filepath = 'tmp/' + CYCLING_FILENAME,
         poke_interval = 10,
-        timeout = 60 * 10 # , fs_conn_id=?
+        timeout = 60 * 10
     )
 
     checking_for_heartrate_file = FileSensor(
         task_id = 'checking_for_heartrate_file',
         filepath = 'tmp/' + HEARTRATE_FILENAME,
         poke_interval = 10,
-        timeout = 60 * 10 # , fs_conn_id=?
+        timeout = 60 * 10
     )
 
     checking_for_walking_running_file = FileSensor(
         task_id = 'checking_for_walking_running_file',
         filepath = 'tmp/' + WALKING_RUNNING_FILENAME,
         poke_interval = 10,
-        timeout = 60 * 10 # , fs_conn_id=?
+        timeout = 60 * 10
     )
     
     insert_cycling_data = PythonOperator(
@@ -194,17 +228,13 @@ with DAG(
         python_callable = insert_walking_running_data
     )
 
-    # TODO: combine tables using SQL and export as a csv file
+    # combine tables using SQL and export as a csv file
     create_table_combined = PostgresOperator(
         task_id = 'combine_tables',
         postgres_conn_id = 'postgres_health_db',
         sql = 'combine_tables.sql'
     )
 
-    # @task()
-    # def export_combined_data():
-    #     df =  pd.read_sql('SELECT *', Connnection.get("postgres_health").get_uri())
-    #     df.to_csv('output/combined_data.csv', index=False)
     export_combined_data = PythonOperator(
         task_id = 'export_combined_data',
         python_callable = export_combined_data
@@ -225,9 +255,9 @@ with DAG(
         bash_command = 'rm /opt/airflow/tmp/{0}'.format(WALKING_RUNNING_FILENAME)
     )
     
-    # create_table_cycling >> checking_for_file >> insert_cycling_data 
     create_table_cycling >> create_table_heartrate >> create_table_walking_running >> create_table_combined_data >> \
-        [checking_for_cycling_file, checking_for_heartrate_file, checking_for_walking_running_file] >> \
+        branch_op >> [parse_xml_data] >> \
+        checking_for_cycling_file >> checking_for_heartrate_file >> checking_for_walking_running_file >> \
         insert_cycling_data >> insert_heartrate_data >> insert_walking_running_data >> \
         create_table_combined >> export_combined_data >> \
         delete_cycling_file >> delete_heartrate_file >> delete_walking_running_file
