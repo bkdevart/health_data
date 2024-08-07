36e8659347447c6786ce67db5488879f526ad849
Brandon Knox <bknox.dev@gmail.com>
2024-08-07 11:36:07 -0700

    restored original pipeline table creations and combined data flow

--- airflow-docker/dags/health_pipeline.py
+++ airflow-docker/dags/health_pipeline.py
@@ -208,36 +208,14 @@ def insert_exercise_time_data():
     conn.close()
     
 def parse_xml_file():
-    # TODO: need to use iterparse method to execute this
     XML_DATA = "tmp/export.xml"
-    # method 1:
-    # # Parse XML file exported from Apple Health app
-    # tree = ET.parse(XML_DATA)
-    # root = tree.getroot()
 
-    # # Store "Record" type data into Pandas.DataFrame
-    # records = [i.attrib for i in root.iter("Record")]
-    # records_df = pd.DataFrame(records)
-    
-    # method 2:
-    # records = []
-
-    # # Iteratively parse the XML file
-    # for event, elem in ET.iterparse(XML_DATA, events=('end',)):
-    #     if elem.tag == "Record" and elem.attrib['type'] == 'HKQuantityTypeIdentifierDistanceCycling':
-    #         records.append(elem.attrib)
-    #     elem.clear()  # Clear the element to save memory
-
-    # # Store "Record" type data into Pandas.DataFrame
-    # records_df = pd.DataFrame(records)
-
-    # method 3:
-    records = []
-    # cycling data
+    # general data (explore other sections of xml later)
+    # TODO: restore sourceVersion, device data by checking for null before adding
     type = [] 
     sourceName = [] 
-    sourceVersion = [] 
-    device = [] 
+    # sourceVersion = [] 
+    # device = [] 
     unit = [] 
     creationDate = []
     startDate = [] 
@@ -245,6 +223,7 @@ def parse_xml_file():
     value = []
 
     # Iteratively parse the XML file
+    # TODO: combine other parse_xml method with this one to optimize run time
     for event, elem in ET.iterparse(XML_DATA, events=('end',)):
         if elem.tag == "Record" and \
             (elem.attrib['type'] == 'HKQuantityTypeIdentifierDistanceCycling' or
@@ -264,10 +243,9 @@ def parse_xml_file():
         elem.clear()  # Clear the element to save memory
 
     li = list(zip(type, sourceName, 
-                #sourceVersion,
+                # sourceVersion,
                 # device, 
-                unit,
-                    creationDate, startDate, endDate, value))
+                unit, creationDate, startDate, endDate, value))
     records_df = pd.DataFrame(li, columns=['type',
                                 'sourceName',
                                 # 'sourceVersion',
@@ -338,107 +316,107 @@ def export_combined_activity_exercise_data():
     df =  pd.read_sql('SELECT * FROM combined_activity_exercise', conn)
     df.to_csv('output/activity_exercise_data.csv', index=False)
 
-# def export_combined_data():
-#     conn = psycopg2.connect(
-#         host="postgres_health",
-#         database="health_db",
-#         user="health_db",
-#         password="health_db"
-#     )
-#     df =  pd.read_sql('SELECT * FROM combined_data', conn)
-#     df.to_csv('output/combined_data.csv', index=False)
+def export_combined_data():
+    conn = psycopg2.connect(
+        host="postgres_health",
+        database="health_db",
+        user="health_db",
+        password="health_db"
+    )
+    df =  pd.read_sql('SELECT * FROM combined_data', conn)
+    df.to_csv('output/combined_data.csv', index=False)
 
-# def insert_cycling_data():
-#     conn = psycopg2.connect(
-#         host="postgres_health",
-#         database="health_db",
-#         user="health_db",
-#         password="health_db"
-#     )
+def insert_cycling_data():
+    conn = psycopg2.connect(
+        host="postgres_health",
+        database="health_db",
+        user="health_db",
+        password="health_db"
+    )
 
-#     cur = conn.cursor()
+    cur = conn.cursor()
 
-#     for file in glob.glob('tmp/' + CYCLING_FILENAME):
-#         df = pd.read_csv(file, usecols=CYCLING_COLS)
+    for file in glob.glob('tmp/' + CYCLING_FILENAME):
+        df = pd.read_csv(file, usecols=CYCLING_COLS)
 
-#         records = df.to_dict('records')
+        records = df.to_dict('records')
         
-#         for record in records:
-#             query = f"""INSERT INTO cycling 
-#                         (start_date, miles, seconds, avg_mph) 
-#                         VALUES (
-#                             '{record['start_date']}', 
-#                             '{record['miles']}', 
-#                             '{record['seconds']}', 
-#                             {record['avg_mph']})
-#                     """
+        for record in records:
+            query = f"""INSERT INTO cycling 
+                        (start_date, miles, seconds, avg_mph) 
+                        VALUES (
+                            '{record['start_date']}', 
+                            '{record['miles']}', 
+                            '{record['seconds']}', 
+                            {record['avg_mph']})
+                    """
 
-#             cur.execute(query)
+            cur.execute(query)
 
-#     conn.commit()
+    conn.commit()
 
-#     cur.close()
-#     conn.close()
+    cur.close()
+    conn.close()
 
-# def insert_heartrate_data():
-#     conn = psycopg2.connect(
-#         host="postgres_health",
-#         database="health_db",
-#         user="health_db",
-#         password="health_db"
-#     )
+def insert_heartrate_data():
+    conn = psycopg2.connect(
+        host="postgres_health",
+        database="health_db",
+        user="health_db",
+        password="health_db"
+    )
 
-#     cur = conn.cursor()
+    cur = conn.cursor()
 
-#     for file in glob.glob('tmp/' + HEARTRATE_FILENAME):
-#         df = pd.read_csv(file, usecols=HEARTRATE_COLS)
+    for file in glob.glob('tmp/' + HEARTRATE_FILENAME):
+        df = pd.read_csv(file, usecols=HEARTRATE_COLS)
 
-#         records = df.to_dict('records')
+        records = df.to_dict('records')
         
-#         for record in records:
-#             query = f"""INSERT INTO heartrate 
-#                         (start_date, beats_per_min) 
-#                         VALUES (
-#                             '{record['start_date']}', 
-#                             '{record['beats_per_min']}')
-#                     """
+        for record in records:
+            query = f"""INSERT INTO heartrate 
+                        (start_date, beats_per_min) 
+                        VALUES (
+                            '{record['start_date']}', 
+                            '{record['beats_per_min']}')
+                    """
 
-#             cur.execute(query)
+            cur.execute(query)
 
-#     conn.commit()
+    conn.commit()
 
-#     cur.close()
-#     conn.close()
+    cur.close()
+    conn.close()
 
-# def insert_walking_running_data():
-#     conn = psycopg2.connect(
-#         host="postgres_health",
-#         database="health_db",
-#         user="health_db",
-#         password="health_db"
-#     )
+def insert_walking_running_data():
+    conn = psycopg2.connect(
+        host="postgres_health",
+        database="health_db",
+        user="health_db",
+        password="health_db"
+    )
 
-#     cur = conn.cursor()
+    cur = conn.cursor()
 
-#     for file in glob.glob('tmp/' + WALKING_RUNNING_FILENAME):
-#         df = pd.read_csv(file, usecols=WALKING_RUNNING_COLS)
+    for file in glob.glob('tmp/' + WALKING_RUNNING_FILENAME):
+        df = pd.read_csv(file, usecols=WALKING_RUNNING_COLS)
 
-#         records = df.to_dict('records')
+        records = df.to_dict('records')
         
-#         for record in records:
-#             query = f"""INSERT INTO walking_running 
-#                         (start_date, miles) 
-#                         VALUES (
-#                             '{record['start_date']}', 
-#                             '{record['miles']}')
-#                     """
+        for record in records:
+            query = f"""INSERT INTO walking_running 
+                        (start_date, miles) 
+                        VALUES (
+                            '{record['start_date']}', 
+                            '{record['miles']}')
+                    """
 
-#             cur.execute(query)
+            cur.execute(query)
 
-#     conn.commit()
+    conn.commit()
 
-#     cur.close()
-#     conn.close()
+    cur.close()
+    conn.close()
 
 with DAG(
     dag_id = 'health_db_pipeline',
@@ -524,106 +502,86 @@ with DAG(
         task_id = 'delete_temp_csv_files',
         bash_command = 'rm -f /opt/airflow/tmp/*.csv'
     )
-
-    create_table_activity_summary >> create_table_exercise_time >> create_table_combined_activity_exercise >>\
-        checking_for_xml_file >> parse_xml_file_old_task >> parse_xml_file_task >> \
-        [checking_for_activity_summary_file, checking_for_excercise_time_file] >> \
-        insert_activity_summary_data_task >> insert_excercise_time_data_task >> \
-            combine_activity_exercise >> export_combined_activity_exercise_data >> \
-            delete_temp_csv_files
     
-    # create_table_cycling = PostgresOperator(
-    #     task_id = 'create_table_cycling',
-    #     postgres_conn_id = 'postgres_health_db',
-    #     sql = 'create_table_cycling.sql'
-    # )
+    create_table_cycling = PostgresOperator(
+        task_id = 'create_table_cycling',
+        postgres_conn_id = 'postgres_health_db',
+        sql = 'create_table_cycling.sql'
+    )
 
-    # create_table_heartrate = PostgresOperator(
-    #     task_id = 'create_table_heartrate',
-    #     postgres_conn_id = 'postgres_health_db',
-    #     sql = 'create_table_heartrate.sql'
-    # )
+    create_table_heartrate = PostgresOperator(
+        task_id = 'create_table_heartrate',
+        postgres_conn_id = 'postgres_health_db',
+        sql = 'create_table_heartrate.sql'
+    )
 
-    # create_table_walking_running = PostgresOperator(
-    #     task_id = 'create_table_walking_running',
-    #     postgres_conn_id = 'postgres_health_db',
-    #     sql = 'create_table_walking_running.sql'
-    # )
+    create_table_walking_running = PostgresOperator(
+        task_id = 'create_table_walking_running',
+        postgres_conn_id = 'postgres_health_db',
+        sql = 'create_table_walking_running.sql'
+    )
 
-    # create_table_combined_data = PostgresOperator(
-    #     task_id = 'create_table_combined_data',
-    #     postgres_conn_id = 'postgres_health_db',
-    #     sql = 'create_table_combined_data.sql'
-    # )
+    create_table_combined_data = PostgresOperator(
+        task_id = 'create_table_combined_data',
+        postgres_conn_id = 'postgres_health_db',
+        sql = 'create_table_combined_data.sql'
+    )
 
-    # checking_for_cycling_file = FileSensor(
-    #     task_id = 'checking_for_cycling_file',
-    #     filepath = 'tmp/' + CYCLING_FILENAME,
-    #     poke_interval = 10,
-    #     timeout = 60 * 10
-    # )
+    checking_for_cycling_file = FileSensor(
+        task_id = 'checking_for_cycling_file',
+        filepath = 'tmp/' + CYCLING_FILENAME,
+        poke_interval = 10,
+        timeout = 60 * 10
+    )
 
-    # checking_for_heartrate_file = FileSensor(
-    #     task_id = 'checking_for_heartrate_file',
-    #     filepath = 'tmp/' + HEARTRATE_FILENAME,
-    #     poke_interval = 10,
-    #     timeout = 60 * 10
-    # )
+    checking_for_heartrate_file = FileSensor(
+        task_id = 'checking_for_heartrate_file',
+        filepath = 'tmp/' + HEARTRATE_FILENAME,
+        poke_interval = 10,
+        timeout = 60 * 10
+    )
 
-    # checking_for_walking_running_file = FileSensor(
-    #     task_id = 'checking_for_walking_running_file',
-    #     filepath = 'tmp/' + WALKING_RUNNING_FILENAME,
-    #     poke_interval = 10,
-    #     timeout = 60 * 10
-    # )
+    checking_for_walking_running_file = FileSensor(
+        task_id = 'checking_for_walking_running_file',
+        filepath = 'tmp/' + WALKING_RUNNING_FILENAME,
+        poke_interval = 10,
+        timeout = 60 * 10
+    )
 
-    # insert_cycling_data = PythonOperator(
-    #     task_id = 'insert_cycling_data',
-    #     python_callable = insert_cycling_data
-    # )
+    insert_cycling_data = PythonOperator(
+        task_id = 'insert_cycling_data',
+        python_callable = insert_cycling_data
+    )
 
-    # insert_heartrate_data = PythonOperator(
-    #     task_id = 'insert_heartrate_data',
-    #     python_callable = insert_heartrate_data
-    # )
+    insert_heartrate_data = PythonOperator(
+        task_id = 'insert_heartrate_data',
+        python_callable = insert_heartrate_data
+    )
 
-    # insert_walking_running_data = PythonOperator(
-    #     task_id = 'insert_walking_running_data',
-    #     python_callable = insert_walking_running_data
-    # )
+    insert_walking_running_data = PythonOperator(
+        task_id = 'insert_walking_running_data',
+        python_callable = insert_walking_running_data
+    )
 
     # combine tables using SQL and export as a csv file
-    # create_table_combined = PostgresOperator(
-    #     task_id = 'combine_tables',
-    #     postgres_conn_id = 'postgres_health_db',
-    #     sql = 'combine_tables.sql'
-    # )
+    create_table_combined = PostgresOperator(
+        task_id = 'combine_tables',
+        postgres_conn_id = 'postgres_health_db',
+        sql = 'combine_tables.sql'
+    )
 
-    # export_combined_data = PythonOperator(
-    #     task_id = 'export_combined_data',
-    #     python_callable = export_combined_data
-    # )
+    export_combined_data = PythonOperator(
+        task_id = 'export_combined_data',
+        python_callable = export_combined_data
+    )
 
-    # delete_cycling_file = BashOperator(
-    #     task_id = 'delete_cycling_file',
-    #     bash_command = 'rm /opt/airflow/tmp/{0}'.format(CYCLING_FILENAME)
-    # )
-
-    # delete_heartrate_file = BashOperator(
-    #     task_id = 'delete_heartrate_file',
-    #     bash_command = 'rm /opt/airflow/tmp/{0}'.format(HEARTRATE_FILENAME)
-    # )
-
-    # delete_walking_running_file = BashOperator(
-    #     task_id = 'delete_walking_running_file',
-    #     bash_command = 'rm /opt/airflow/tmp/{0}'.format(WALKING_RUNNING_FILENAME)
-    # )
-    
-    # create_table_activity_summary >> create_table_cycling >> create_table_heartrate >> \
-    #     create_table_walking_running >> create_table_combined_data >> \
-    #     checking_for_xml_file >> parse_xml_file_task >> checking_for_activity_summary_file >> checking_for_excercise_time_file >> \
-    #     insert_activity_summary_data_task >> \
-    #     [checking_for_cycling_file, checking_for_heartrate_file, checking_for_walking_running_file] >> \
-    #     insert_cycling_data >> insert_heartrate_data >> insert_walking_running_data >> \
-    #     create_table_combined >> export_combined_data >> \
-    #     delete_cycling_file >> delete_heartrate_file >> delete_walking_running_file
+    create_table_activity_summary >> create_table_exercise_time >> create_table_combined_activity_exercise >> \
+    create_table_cycling >> create_table_heartrate >> \
+    create_table_walking_running >> create_table_combined_data >> \
+    checking_for_xml_file >> parse_xml_file_old_task >> parse_xml_file_task >> \
+    [checking_for_activity_summary_file, checking_for_excercise_time_file, checking_for_cycling_file, checking_for_heartrate_file, checking_for_walking_running_file] >> \
+    insert_activity_summary_data_task >> insert_excercise_time_data_task >> \
+        insert_cycling_data >> insert_heartrate_data >> insert_walking_running_data >> \
+        create_table_combined >> export_combined_data >> \
+        combine_activity_exercise >> export_combined_activity_exercise_data >> \
+        delete_temp_csv_files
