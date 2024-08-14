2753e3da99fcd69ede3f49a12bff6a9553c4cd3a
Brandon Knox <bknox.dev@gmail.com>
2024-08-14 15:28:10 -0700

    Import code added to fact notebook, started testing sql joins with new data structure

--- sql/create_table_fact_health_activity.sql
+++ sql/create_table_fact_health_activity.sql
@@ -5,9 +5,15 @@ CREATE TABLE IF NOT EXISTS fact_health_activity (
     start_date DATE,
     source_name VARCHAR,
     unit VARCHAR,
-    value VARCHAR,
+    value FLOAT,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
     FOREIGN KEY (customer_id) REFERENCES dim_customer (customer_id),
     FOREIGN KEY (activity_type_id) REFERENCES dim_activity_type (activity_type_id)
 );
+
+-- SELECT customer_id, activity_name, start_date, unit, SUM(value)
+-- FROM fact_health_activity
+-- LEFT JOIN dim_activity_type USING (activity_type_id)
+-- GROUP BY customer_id, activity_name, start_date, unit
+-- LIMIT 5;
